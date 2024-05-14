61bcdd3ce77b198af606bc567b73a125e47de470
David Whiting <david.g.whiting@gmail.com>
2024-05-14 11:30:28 -0500

    renamed data to courtlistener

--- old/resources/make_user.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker exec -it law-postgres psql -U postgres -c "CREATE ROLE django WITH LOGIN PASSWORD 'djangopassword';"
-
--- old/resources/run1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#docker exec -it law-postgres psql -U postgres -c "CREATE ROLE django WITH LOGIN PASSWORD 'djangopassword';"
-#docker exec -it law-postgres psql -U postgres -c "CREATE DATABASE courtlistener WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.UTF-8';"
-
